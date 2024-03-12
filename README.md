3aa312a20bba863f1798c519b6b6557b37fb7db6
yangheng <1426067124@qq.com>
2024-03-12 16:41:22 +0800

    ✨ feat(dependences): 安装antd

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ```bash
 pnpm create vite react-admin --template react-ts
 ```
+
+2. 安装依赖
+
+2.1 `antd`
+
+```bash
+pnpm add antd @ant-design/icons
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "antd";
+import { ArrowDownOutlined } from "@ant-design/icons";
+
 function App() {
   return (
     <div>
       <h1>Hello, world!</h1>
+      <Button type="primary" icon={<ArrowDownOutlined />}>
+        Primary Button
+      </Button>
     </div>
   );
 }
